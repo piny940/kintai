840af873620a155bb12102daa7650bfd24de42da
mikan <83708535+piny940@users.noreply.github.com>
2023-10-13 10:39:38 +0900

    create view

--- db/commands/create_view.sql
+++ db/commands/create_view.sql
@@ -1,6 +1,6 @@
 create view employments_view (id, name, worker_id) as 
   select companies.id, name, worker_id from companies
-    inner join affiliations on companies.id = affiliations.company_id;
+    inner join employments on companies.id = employments.company_id;
 
 select first_name, last_name from workers
   inner join employments_view on workers.id = employments_view.worker_id
