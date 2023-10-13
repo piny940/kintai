b169c6e39c138439c985bacbb786e90c5b771011
mikan <83708535+piny940@users.noreply.github.com>
2023-10-13 10:34:45 +0900

    sub query

--- db/commands/sub_query.sql
+++ db/commands/sub_query.sql
@@ -1,4 +1,4 @@
 select workers.first_name, workers.last_name from workers
   where workers.id in
-    (select worker_id from affiliations
+    (select worker_id from employments
       where company_id = 2);
