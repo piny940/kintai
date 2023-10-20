df7c28060ab693162879cf77961d25b4a442ca4f
mikan <83708535+piny940@users.noreply.github.com>
2023-10-20 15:31:45 +0900

    打刻できるようにする

--- frontend/src/containers/App.tsx
+++ frontend/src/containers/App.tsx
@@ -1,12 +1,33 @@
 import { TestID } from '@/resources/TestID'
 import CompanySelect from './CompanySelect'
+import { postData } from '@/utils/api'
+import { useWorkerInfo } from '@/context/WorkerInfoProvider'
+import { useCallback } from 'react'
 
 export const App: React.FC = () => {
+  const { company } = useWorkerInfo()
+
+  const createStamp = useCallback(async () => {
+    if (!company) return
+
+    await postData({
+      url: `/member/companies/${company.id}/stamps/now`,
+      data: {},
+    })
+  }, [company])
+
   return (
     <div id="app" data-testid={TestID.APP}>
       <h1>勤怠プラス+</h1>
       <div className="container">
         <CompanySelect />
+        {company && (
+          <div className="mt-5">
+            <button className="btn btn-primary btn-lg" onClick={createStamp}>
+              打刻する
+            </button>
+          </div>
+        )}
       </div>
     </div>
   )
