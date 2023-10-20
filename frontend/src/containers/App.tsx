6262b03d9855b3ae866f3b1b796e68f70175d379
mikan <83708535+piny940@users.noreply.github.com>
2023-10-20 16:14:41 +0900

    WorkStatusが表示されるようにする

--- frontend/src/containers/App.tsx
+++ frontend/src/containers/App.tsx
@@ -1,11 +1,13 @@
 import { TestID } from '@/resources/TestID'
 import CompanySelect from './CompanySelect'
-import { postData } from '@/utils/api'
+import { getData, postData } from '@/utils/api'
 import { useWorkerInfo } from '@/context/WorkerInfoProvider'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import { WorkStatus, workStatusLabels } from '@/resources/types'
 
 export const App: React.FC = () => {
   const { company } = useWorkerInfo()
+  const [workStatus, setWorkStatus] = useState<WorkStatus | null>(null)
 
   const createStamp = useCallback(async () => {
     if (!company) return
@@ -16,6 +18,20 @@ export const App: React.FC = () => {
     })
   }, [company])
 
+  const fetchWorkStatus = useCallback(async () => {
+    if (!company) return
+
+    const json = (
+      await getData(`/member/companies/${company.id}/work_status`)
+    )[1]
+
+    setWorkStatus(json.work_status)
+  }, [company])
+
+  useEffect(() => {
+    void fetchWorkStatus()
+  }, [fetchWorkStatus])
+
   return (
     <div id="app" data-testid={TestID.APP}>
       <h1>勤怠プラス+</h1>
@@ -23,12 +39,17 @@ export const App: React.FC = () => {
         <CompanySelect />
         {company && (
           <div className="mt-5">
-            <button
-              className="btn btn-primary btn-lg w-100 py-5 fs-2"
-              onClick={createStamp}
-            >
-              打刻する
-            </button>
+            <div className="stamp">
+              <button
+                className="btn btn-primary btn-lg w-100 py-5 fs-2"
+                onClick={createStamp}
+              >
+                打刻する
+              </button>
+            </div>
+            <div className="work-status">
+              {workStatus != null ? workStatusLabels[workStatus] : '---'}
+            </div>
           </div>
         )}
       </div>
