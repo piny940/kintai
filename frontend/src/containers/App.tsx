5317086eac80cb4ef51d812d51eec9e5a5b148ef
mikan <83708535+piny940@users.noreply.github.com>
2023-10-20 14:58:36 +0900

    ログインを強制する仕組みをlayoutに記述

--- frontend/src/containers/App.tsx
+++ frontend/src/containers/App.tsx
@@ -1,17 +1,6 @@
-import { useWorkerInfo } from '@/context/WorkerInfoProvider'
 import { TestID } from '@/resources/TestID'
-import { useEffect } from 'react'
 
 export const App: React.FC = () => {
-  const { worker, loading } = useWorkerInfo()
-
-  useEffect(() => {
-    if (loading) return
-    if (worker) return
-    console.log('ログインしてください')
-  }, [worker, loading])
-
-  if (loading) return <div>Loading...</div>
   return (
     <div id="app" data-testid={TestID.APP}>
       <h1>勤怠プラス+</h1>
