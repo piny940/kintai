825ed41fec35472fd8d3d84eb2d1bd36b695df54
mikan <83708535+piny940@users.noreply.github.com>
2023-10-20 10:42:33 +0900

    Worker, Company型を定義

--- frontend/src/resources/types.ts
+++ frontend/src/resources/types.ts
@@ -1,1 +1,23 @@
 export type Theme = 'dark' | 'light'
+
+export enum WorkerKind {
+  ADMIN = 0,
+  MEMBER = 1,
+}
+enum WorkerStatus {
+  ACTIVE = 0,
+  INACTIVE = 1,
+}
+
+export type Worker = {
+  id: number
+  kind: WorkerKind
+  status: WorkerStatus
+  email: string
+  firstName: string
+  lastName: string
+}
+export type Company = {
+  id: number
+  name: string
+}
