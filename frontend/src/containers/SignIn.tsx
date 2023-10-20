5048fb626b7e2351fe3e8008c6499bd3213593b4
mikan <83708535+piny940@users.noreply.github.com>
2023-10-20 11:08:53 +0900

    フォームの値をstateで管理

--- frontend/src/containers/SignIn.tsx
+++ frontend/src/containers/SignIn.tsx
@@ -1,6 +1,9 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
 
 const SignIn = (): JSX.Element => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className="container">
       <h1>ログイン</h1>
@@ -8,13 +11,23 @@ const SignIn = (): JSX.Element => {
         <label className="form-group row my-2">
           <div className="col-form-label col-md-3">メールアドレス</div>
           <div className="col-md-9">
-            <input type="email" className="form-control" />
+            <input
+              type="email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
         </label>
         <label className="form-group row my-2">
           <div className="col-form-label col-md-3">パスワード</div>
           <div className="col-md-9">
-            <input type="password" className="form-control" />
+            <input
+              type="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
         </label>
         <div className="row my-2">
