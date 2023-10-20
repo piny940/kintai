2fd3934971cc7d5305ae59bef3cabc0a4b74dc8f
mikan <83708535+piny940@users.noreply.github.com>
2023-10-20 10:54:47 +0900

    不要なコードを削除

--- frontend/src/containers/App.tsx
+++ frontend/src/containers/App.tsx
@@ -1,25 +1,6 @@
-import { useWorkerInfo } from '@/context/WorkerInfoProvider'
 import { TestID } from '@/resources/TestID'
-import { fetchApi, postData } from '@/utils/api'
-import { useEffect } from 'react'
 
 export const App: React.FC = () => {
-  const { worker, company, setWorker } = useWorkerInfo()
-
-  const login = async () => {
-    const json = await postData({
-      url: '/sessions',
-      data: {
-        email: 'foo1@example.com',
-        password: 'password',
-      },
-    })
-    console.log(json)
-  }
-  console.log(worker)
-  useEffect(() => {
-    // void login()
-  })
   return (
     <div id="app" data-testid={TestID.APP}>
       <h1>勤怠プラス+</h1>
