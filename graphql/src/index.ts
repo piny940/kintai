0a3369f88823004760aea68939c5300c0e6aacc7
mikan <83708535+piny940@users.noreply.github.com>
2023-10-26 11:59:27 +0900

    graphqlのサンプルを作成

---
+++ graphql/src/index.ts
@@ -0,0 +1,45 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { addResolversToSchema } from '@graphql-tools/schema';
+
+const schema = loadSchemaSync('src/index.graphql', {
+  loaders: [new GraphQLFileLoader()],
+})
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const schemaWithResolvers = addResolversToSchema({
+  schema,
+  resolvers,
+})
+
+const server = new ApolloServer({
+  schema: schemaWithResolvers
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
