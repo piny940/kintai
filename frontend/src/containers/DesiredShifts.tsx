67c1d61ec91ae8f5f1a9319a7eac8c0144df2a6b
mikan <83708535+piny940@users.noreply.github.com>
2023-10-26 14:35:44 +0900

    カレンダーを配置

--- frontend/src/containers/DesiredShifts.tsx
+++ frontend/src/containers/DesiredShifts.tsx
@@ -1,7 +1,17 @@
 import { memo } from 'react'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
 
 const DesiredShifts = (): JSX.Element => {
-  return <div>DesiredShifts</div>
+  return (
+    <div>
+      <Calendar
+        allowPartialRange
+        className={['w-100']}
+        tileClassName={['pb-5']}
+      />
+    </div>
+  )
 }
 
 export default memo(DesiredShifts)
