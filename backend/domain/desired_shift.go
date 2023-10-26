3d0094fa6cf6d2ce97c374fab74c49289cd4d393
mikan <83708535+piny940@users.noreply.github.com>
2023-10-26 13:46:07 +0900

    desiredShiftRepoを作成

--- backend/domain/desired_shift.go
+++ backend/domain/desired_shift.go
@@ -7,15 +7,20 @@ type DesiredShiftID int
 type DesiredShift struct {
 	ID           DesiredShiftID `json:"id"`
 	Since        time.Time      `json:"since"`
-	Until        time.Time      `json:"until"`
+	Till         time.Time      `json:"till"`
 	EmploymentID EmploymentID   `json:"employment_id"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+}
+type IDesiredShiftRepo interface {
+	List(employmentId EmploymentID) ([]*DesiredShift, error)
+	Create(desiredShift *DesiredShift) (*DesiredShift, error)
 }
 
-func NewDesiredShift(id DesiredShiftID, since time.Time, until time.Time, employmentID EmploymentID) *DesiredShift {
+func NewDesiredShift(since time.Time, till time.Time, employmentID EmploymentID) *DesiredShift {
 	return &DesiredShift{
-		ID:           id,
 		Since:        since,
-		Until:        until,
+		Till:         till,
 		EmploymentID: employmentID,
 	}
 }
