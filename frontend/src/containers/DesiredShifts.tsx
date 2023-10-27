f5666f93ee89f3b3ccfbe12c51c1e1bef3a99a20
mikan <83708535+piny940@users.noreply.github.com>
2023-10-27 09:07:30 +0900

    パスを修正

--- frontend/src/containers/DesiredShifts.tsx
+++ frontend/src/containers/DesiredShifts.tsx
@@ -1,10 +1,30 @@
 import DesiredShiftsCalendar from '@/components/Kintai/DesiredShiftsCalendar'
-import { memo, useState } from 'react'
+import { useWorkerInfo } from '@/context/WorkerInfoProvider'
+import { DesiredShift } from '@/resources/types'
+import { getData } from '@/utils/api'
+import { memo, useCallback, useEffect, useState } from 'react'
 import 'react-calendar/dist/Calendar.css'
 
 const DesiredShifts = (): JSX.Element => {
+  const { worker, loading, company } = useWorkerInfo()
   const [alert, setAlert] = useState('')
-  const [desiredShifts, setDesiredShifts] = useState([])
+  const [desiredShifts, setDesiredShifts] = useState<DesiredShift[]>([])
+
+  const pullDesiredShifts = useCallback(async () => {
+    if (!company) return
+    const [res, json] = await getData(`/companies/${company.id}/desired_shifts`)
+    if (!res.ok) {
+      setAlert(json.message)
+      console.log(json.message)
+      return
+    }
+    setDesiredShifts(json.desired_shifts)
+    console.log(json.desired_shifts)
+  }, [company])
+
+  useEffect(() => {
+    void pullDesiredShifts()
+  }, [])
 
   return (
     <div>
