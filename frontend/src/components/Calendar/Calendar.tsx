c1312e1643b40c164f2402918205247ee610142c
piny940 <an.s.shoma@gmail.com>
2023-10-31 23:17:12 +0900

    カレンダーで月を移動できるようにする

--- frontend/src/components/Calendar/Calendar.tsx
+++ frontend/src/components/Calendar/Calendar.tsx
@@ -15,22 +15,44 @@ const Calendar = ({
   yearMonth,
   setYearMonth,
 }: CalendarProps): JSX.Element => {
-  const { year, month, datesMatrix } = useCalendar({ yearMonth, setYearMonth })
+  const { year, month, datesMatrix, goNextMonth, goPrevMonth } = useCalendar({
+    yearMonth,
+    setYearMonth,
+  })
 
   return (
-    <table className="table table-striped">
-      <thead className="flex flex-1">
-        <WeekDays />
-      </thead>
-      <tbody>
-        <Month
-          renderDate={renderDate}
-          year={year}
-          month={month}
-          datesMatrix={datesMatrix}
-        />
-      </tbody>
-    </table>
+    <div className="calendar">
+      <div className="d-flex justify-content-around mb-2">
+        <button
+          className="btn btn-outline-primary btn-sm"
+          onClick={goPrevMonth}
+        >
+          前月
+        </button>
+        <span className="h3 m-0">
+          {year}年{month + 1}月
+        </span>
+        <button
+          className="btn btn-outline-primary btn-sm"
+          onClick={goNextMonth}
+        >
+          翌月
+        </button>
+      </div>
+      <table className="table table-striped">
+        <thead className="flex flex-1">
+          <WeekDays />
+        </thead>
+        <tbody>
+          <Month
+            renderDate={renderDate}
+            year={year}
+            month={month}
+            datesMatrix={datesMatrix}
+          />
+        </tbody>
+      </table>
+    </div>
   )
 }
 export default memo(Calendar)
