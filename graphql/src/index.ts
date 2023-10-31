1598eaf2cc1528994591337c631604fa2bfe6527
mikan <83708535+piny940@users.noreply.github.com>
2023-10-31 23:22:01 +0900

    tmp

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -3,17 +3,32 @@ import { startStandaloneServer } from '@apollo/server/standalone'
 import { loadSchemaSync } from '@graphql-tools/load'
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { addResolversToSchema } from '@graphql-tools/schema'
+import { serialize } from 'object-to-formdata'
 
 const schema = loadSchemaSync('src/index.graphql', {
   loaders: [new GraphQLFileLoader()],
 })
 
-const HOST = 'http://localhost:8080'
+const HOST = 'http://localhost:8080/v1'
 
 const resolvers = {
   Query: {
     worker: async () => {
-      const response = await fetch(`${HOST}/workers/me`)
+      const response = await fetch(`${HOST}/workers/me`, {
+        credentials: 'include',
+      })
+      const json = (await response.json()) as any
+      console.log(json)
+      return json.worker
+    },
+  },
+  Mutation: {
+    login: async (_, args: { email: string; password: string }) => {
+      const response = await fetch(`${HOST}/session`, {
+        method: 'POST',
+        body: serialize({ email: args.email, password: args.password }),
+        credentials: 'include',
+      })
       const json = (await response.json()) as any
       return json.worker
     },
