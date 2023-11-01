eed075c9f55ed5ffd1fd6885becc39407524e988
piny940 <an.s.shoma@gmail.com>
2023-11-01 17:21:04 +0900

    サーバーはechoを使う

---
+++ backend/server/graphql.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"kintai_backend/graph"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/labstack/echo/v4"
+)
+
+func graphqlHandler() echo.HandlerFunc {
+	graphql := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	
+	return func(c echo.Context) error {
+		graphql.ServeHTTP(c.Response(), c.Request())
+		return nil
+	}
+}
+func playgroundHandler() echo.HandlerFunc {
+	handler := playground.Handler("GraphQL playground", "/query")
+
+	return func(c echo.Context) error {
+		handler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	}
+}
--- backend/server/init.go
+++ backend/server/init.go
@@ -3,15 +3,20 @@ package server
 import (
 	"kintai_backend/config"
 
+	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func Init() error {
 	c := config.GetConfig()
-	echo, err := NewRouter()
-	if err != nil {
-		return err
-	}
+	echo := echo.New()
+	// echo, err := NewRouter()
+	// if err != nil {
+	// 	return err
+	// }
+	
+	echo.POST("/query", graphqlHandler())
+	echo.GET("/", playgroundHandler())
 	echo.Use(middleware.Logger())
 	echo.Use(middleware.Recover())
 
