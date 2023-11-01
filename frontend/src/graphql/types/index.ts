dfd712adcc338c1e7da1967678f4b6bab08e026f
piny940 <an.s.shoma@gmail.com>
2023-11-01 22:33:49 +0900

    クエリを修正

--- frontend/src/graphql/types/index.ts
+++ frontend/src/graphql/types/index.ts
@@ -16,12 +16,15 @@ export type Scalars = {
   Int: { input: number; output: number; }
   Float: { input: number; output: number; }
   Time: { input: any; output: any; }
+  Uint: { input: any; output: any; }
 };
 
 export type Company = {
   __typename?: 'Company';
   createdAt: Scalars['Time']['output'];
-  id: Scalars['Int']['output'];
+  employment?: Maybe<Employment>;
+  employmentId?: Maybe<Scalars['Uint']['output']>;
+  id: Scalars['Uint']['output'];
   name: Scalars['String']['output'];
   updatedAt: Scalars['Time']['output'];
 };
@@ -30,7 +33,7 @@ export type DesiredShift = {
   __typename?: 'DesiredShift';
   createdAt: Scalars['Time']['output'];
   employmentId: Scalars['Int']['output'];
-  id: Scalars['Int']['output'];
+  id: Scalars['Uint']['output'];
   since: Scalars['String']['output'];
   till: Scalars['String']['output'];
   updatedAt: Scalars['Time']['output'];
@@ -40,7 +43,7 @@ export type Employment = {
   __typename?: 'Employment';
   companyId: Scalars['Int']['output'];
   createdAt: Scalars['Time']['output'];
-  id: Scalars['Int']['output'];
+  id: Scalars['Uint']['output'];
   kind: EmploymentKind;
   status: EmploymentStatus;
   updatedAt: Scalars['Time']['output'];
@@ -76,7 +79,14 @@ export type MutationLoginArgs = {
 
 export type Query = {
   __typename?: 'Query';
+  companies?: Maybe<Array<Company>>;
+  company?: Maybe<Company>;
   me?: Maybe<Worker>;
+};
+
+
+export type QueryCompanyArgs = {
+  id: Scalars['Uint']['input'];
 };
 
 export enum WorkStatus {
@@ -88,10 +98,11 @@ export type Worker = {
   __typename?: 'Worker';
   createdAt: Scalars['Time']['output'];
   email: Scalars['String']['output'];
-  id: Scalars['Int']['output'];
+  id: Scalars['Uint']['output'];
   name: WorkerName;
   status: WorkerStatus;
   updatedAt: Scalars['Time']['output'];
+  workStatus?: Maybe<WorkStatus>;
 };
 
 export type WorkerName = {
@@ -105,15 +116,131 @@ export enum WorkerStatus {
   Inactive = 'INACTIVE'
 }
 
+export type GetCompanyQueryVariables = Exact<{
+  id: Scalars['Uint']['input'];
+}>;
+
+
+export type GetCompanyQuery = { __typename?: 'Query', company?: { __typename?: 'Company', id: any, name: string, createdAt: any, updatedAt: any, employment?: { __typename?: 'Employment', id: any, kind: EmploymentKind, status: EmploymentStatus, createdAt: any, updatedAt: any } | null } | null };
+
+export type GetCompaniesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetCompaniesQuery = { __typename?: 'Query', companies?: Array<{ __typename?: 'Company', id: any, name: string, createdAt: any, updatedAt: any, employment?: { __typename?: 'Employment', id: any, kind: EmploymentKind, status: EmploymentStatus, createdAt: any, updatedAt: any } | null }> | null };
+
 export type LoginMutationVariables = Exact<{
   email: Scalars['String']['input'];
   password: Scalars['String']['input'];
 }>;
 
 
-export type LoginMutation = { __typename?: 'Mutation', login?: { __typename?: 'LoginResponse', worker?: { __typename?: 'Worker', id: number, status: WorkerStatus, email: string, createdAt: any, updatedAt: any, name: { __typename?: 'WorkerName', firstName: string, lastName: string } } | null } | null };
+export type LoginMutation = { __typename?: 'Mutation', login?: { __typename?: 'LoginResponse', worker?: { __typename?: 'Worker', id: any, status: WorkerStatus, email: string, createdAt: any, updatedAt: any, name: { __typename?: 'WorkerName', firstName: string, lastName: string } } | null } | null };
+
+export type GetMeQueryVariables = Exact<{ [key: string]: never; }>;
 
 
+export type GetMeQuery = { __typename?: 'Query', me?: { __typename?: 'Worker', id: any, status: WorkerStatus, email: string, createdAt: any, updatedAt: any, name: { __typename?: 'WorkerName', firstName: string, lastName: string } } | null };
+
+
+export const GetCompanyDocument = gql`
+    query getCompany($id: Uint!) {
+  company(id: $id) {
+    id
+    name
+    employment {
+      id
+      kind
+      status
+      createdAt
+      updatedAt
+    }
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+/**
+ * __useGetCompanyQuery__
+ *
+ * To run a query within a React component, call `useGetCompanyQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetCompanyQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetCompanyQuery({
+ *   variables: {
+ *      id: // value for 'id'
+ *   },
+ * });
+ */
+export function useGetCompanyQuery(baseOptions: Apollo.QueryHookOptions<GetCompanyQuery, GetCompanyQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetCompanyQuery, GetCompanyQueryVariables>(GetCompanyDocument, options);
+      }
+export function useGetCompanyLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetCompanyQuery, GetCompanyQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetCompanyQuery, GetCompanyQueryVariables>(GetCompanyDocument, options);
+        }
+export function useGetCompanySuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<GetCompanyQuery, GetCompanyQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetCompanyQuery, GetCompanyQueryVariables>(GetCompanyDocument, options);
+        }
+export type GetCompanyQueryHookResult = ReturnType<typeof useGetCompanyQuery>;
+export type GetCompanyLazyQueryHookResult = ReturnType<typeof useGetCompanyLazyQuery>;
+export type GetCompanySuspenseQueryHookResult = ReturnType<typeof useGetCompanySuspenseQuery>;
+export type GetCompanyQueryResult = Apollo.QueryResult<GetCompanyQuery, GetCompanyQueryVariables>;
+export const GetCompaniesDocument = gql`
+    query getCompanies {
+  companies {
+    id
+    name
+    employment {
+      id
+      kind
+      status
+      createdAt
+      updatedAt
+    }
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+/**
+ * __useGetCompaniesQuery__
+ *
+ * To run a query within a React component, call `useGetCompaniesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetCompaniesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetCompaniesQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetCompaniesQuery(baseOptions?: Apollo.QueryHookOptions<GetCompaniesQuery, GetCompaniesQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetCompaniesQuery, GetCompaniesQueryVariables>(GetCompaniesDocument, options);
+      }
+export function useGetCompaniesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetCompaniesQuery, GetCompaniesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetCompaniesQuery, GetCompaniesQueryVariables>(GetCompaniesDocument, options);
+        }
+export function useGetCompaniesSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<GetCompaniesQuery, GetCompaniesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetCompaniesQuery, GetCompaniesQueryVariables>(GetCompaniesDocument, options);
+        }
+export type GetCompaniesQueryHookResult = ReturnType<typeof useGetCompaniesQuery>;
+export type GetCompaniesLazyQueryHookResult = ReturnType<typeof useGetCompaniesLazyQuery>;
+export type GetCompaniesSuspenseQueryHookResult = ReturnType<typeof useGetCompaniesSuspenseQuery>;
+export type GetCompaniesQueryResult = Apollo.QueryResult<GetCompaniesQuery, GetCompaniesQueryVariables>;
 export const LoginDocument = gql`
     mutation Login($email: String!, $password: String!) {
   login(email: $email, password: $password) {
@@ -158,3 +285,50 @@ export function useLoginMutation(baseOptions?: Apollo.MutationHookOptions<LoginM
 export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
 export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
 export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
+export const GetMeDocument = gql`
+    query getMe {
+  me {
+    id
+    status
+    email
+    name {
+      firstName
+      lastName
+    }
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+/**
+ * __useGetMeQuery__
+ *
+ * To run a query within a React component, call `useGetMeQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetMeQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetMeQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetMeQuery(baseOptions?: Apollo.QueryHookOptions<GetMeQuery, GetMeQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetMeQuery, GetMeQueryVariables>(GetMeDocument, options);
+      }
+export function useGetMeLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetMeQuery, GetMeQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetMeQuery, GetMeQueryVariables>(GetMeDocument, options);
+        }
+export function useGetMeSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<GetMeQuery, GetMeQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetMeQuery, GetMeQueryVariables>(GetMeDocument, options);
+        }
+export type GetMeQueryHookResult = ReturnType<typeof useGetMeQuery>;
+export type GetMeLazyQueryHookResult = ReturnType<typeof useGetMeLazyQuery>;
+export type GetMeSuspenseQueryHookResult = ReturnType<typeof useGetMeSuspenseQuery>;
+export type GetMeQueryResult = Apollo.QueryResult<GetMeQuery, GetMeQueryVariables>;
