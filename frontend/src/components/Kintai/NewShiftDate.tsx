6417f00f94ec0346aea82dae32785e8e4a4952db
piny940 <an.s.shoma@gmail.com>
2023-11-02 16:17:38 +0900

    NewShiftsDateを作成

---
+++ frontend/src/components/Kintai/NewShiftDate.tsx
@@ -0,0 +1,58 @@
+import { Dayjs } from 'dayjs'
+import { memo } from 'react'
+import ShiftItem from './ShiftItem'
+import { DesiredShift } from '@/graphql/types'
+
+export type NewShiftsDateProps = {
+  month: number
+  date: Dayjs
+  onAddButtonClicked: () => void
+  desiredShifts: DesiredShift[]
+}
+
+const NewShiftsDate = ({
+  date,
+  month,
+  onAddButtonClicked,
+  desiredShifts,
+}: NewShiftsDateProps): JSX.Element => {
+  const textColor = (): string => {
+    if (date.month() !== month) {
+      return 'text-secondary'
+    }
+    if (date.day() === 0) {
+      return 'text-danger'
+    }
+    return ''
+  }
+  const isCurrentMonth = month === date.month()
+
+  return (
+    <div className="">
+      <div className="d-flex align-items-center">
+        <span className={textColor()}>{date.date()}</span>
+        {isCurrentMonth && (
+          <button
+            className="mt-2 btn btn-outline-primary ms-2 btn-sm"
+            onClick={onAddButtonClicked}
+          >
+            追加
+          </button>
+        )}
+      </div>
+      {isCurrentMonth && (
+        <ul className="list-unstyled">
+          {desiredShifts.map((desiredShift) => (
+            <ShiftItem
+              className="btn btn-outlined"
+              key={desiredShift.id}
+              shift={desiredShift}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
+
+export default memo(NewShiftsDate)
