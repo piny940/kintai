83cc463a5116efa0105c1d49d44bbe69446fe4ce
piny940 <an.s.shoma@gmail.com>
2023-11-02 15:51:19 +0900

    calendarを表示

--- frontend/src/containers/NewShifts.tsx
+++ frontend/src/containers/NewShifts.tsx
@@ -1,9 +1,32 @@
-import { memo } from 'react'
+import Calendar from '@/components/Calendar/Calendar'
+import { useGetCompanyLazyQuery } from '@/graphql/types'
+import { useCompanyId } from '@/utils/hooks'
+import dayjs, { Dayjs } from 'dayjs'
+import Error from 'next/error'
+import { memo, useEffect, useState } from 'react'
 
 const NewShifts = (): JSX.Element => {
+  const companyId = useCompanyId()
+  const [loadCompany, { data: companyData, error }] = useGetCompanyLazyQuery()
+  const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null)
+  const [selectedMonth, setSelectedMonth] = useState<Dayjs>(dayjs(Date.now()))
+
+  useEffect(() => {
+    if (!companyId) return
+    void loadCompany({ variables: { id: companyId } })
+  }, [companyId, loadCompany])
+
+  if (error) return <Error statusCode={404} />
+  if (!companyData?.company) return <>loading...</>
   return (
     <div>
       <h1>シフト作成</h1>
+      <h2 className="ms-2 mb-4">{companyData.company.name}</h2>
+      <Calendar
+        yearMonth={selectedMonth}
+        setYearMonth={setSelectedMonth}
+        renderDate={() => <div>test</div>}
+      />
     </div>
   )
 }
