a97509f160df0089f3cdb7f7f797ad140accbbbf
piny940 <an.s.shoma@gmail.com>
2023-11-03 22:50:24 +0900

    ShiftCheckBoxを作成

--- frontend/src/components/Kintai/ShiftsDisplayToggler.tsx
+++ frontend/src/components/Kintai/ShiftsDisplayToggler.tsx
@@ -1,3 +1,4 @@
+import ShiftCheckBox from './ShiftCheckBox'
 import ShiftItem from './ShiftItem'
 import ShiftItemButton from './ShiftItemButton'
 
@@ -18,41 +19,43 @@ const ShiftsDisplayToggler = ({
     <div className="rounded border px-4 py-2 bg-body-tertiary">
       <h2 className="h5 mb-0">表示</h2>
       <div className="row">
-        <div className="col col-lg-4 d-flex flex-wrap mb-1 align-items-center">
-          <span className="mx-2">希望シフト</span>
-          <div className="">
-            <ShiftItemButton
-              className="btn-outline-info text-body d-inline-block"
-              onClick={() => undefined}
-              shift={{
-                since: '2023-11-10T00:00+09:00',
-                till: '2023-11-10T00:00+09:00',
-                employment: {
-                  worker: {
-                    name: { firstName: 'xx', lastName: 'xx' },
-                  },
+        <ShiftCheckBox
+          label="希望シフト"
+          checked={showDesiredShifts}
+          toggle={toggleShowDesiredShifts}
+        >
+          <ShiftItemButton
+            className="btn-outline-info text-body d-inline-block"
+            onClick={() => undefined}
+            shift={{
+              since: '2023-11-10T00:00+09:00',
+              till: '2023-11-10T00:00+09:00',
+              employment: {
+                worker: {
+                  name: { firstName: 'xx', lastName: 'xx' },
                 },
-              }}
-            />
-          </div>
-        </div>
-        <div className="col col-lg-4 d-flex flex-wrap mb-1 align-items-center">
-          <span className="mx-2">シフト</span>
-          <div className="">
-            <ShiftItem
-              className="bg-info d-inline-block"
-              shift={{
-                since: '2023-11-10T00:00+09:00',
-                till: '2023-11-10T00:00+09:00',
-                employment: {
-                  worker: {
-                    name: { firstName: 'xx', lastName: 'xx' },
-                  },
+              },
+            }}
+          />
+        </ShiftCheckBox>
+        <ShiftCheckBox
+          label="シフト"
+          checked={showShifts}
+          toggle={toggleShowShifts}
+        >
+          <ShiftItem
+            className="bg-info d-inline-block"
+            shift={{
+              since: '2023-11-10T00:00+09:00',
+              till: '2023-11-10T00:00+09:00',
+              employment: {
+                worker: {
+                  name: { firstName: 'xx', lastName: 'xx' },
                 },
-              }}
-            />
-          </div>
-        </div>
+              },
+            }}
+          />
+        </ShiftCheckBox>
       </div>
     </div>
   )
