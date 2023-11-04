ce0151f25645f1ce30b4671df4421d7644a17482
piny940 <an.s.shoma@gmail.com>
2023-11-04 10:43:49 +0900

    NewStampServiceを修正

--- backend/domain/stamp_service.go
+++ backend/domain/stamp_service.go
@@ -7,50 +7,71 @@ type StampService struct {
 	fromTime     time.Time
 	toTime       time.Time
 	stampRepo    IStampRepo
+	stamps       []*Stamp
 }
 
-func NewStampService(employmentId EmploymentID, since time.Time, until time.Time, stampRepo IStampRepo) *StampService {
+func NewStampService(employmentId EmploymentID, since, until time.Time, stampRepo IStampRepo) (*StampService, error) {
+	stamps, err := stampRepo.List(&StampQuery{
+		EmploymentId: &employmentId,
+		FromTime:     &since,
+		ToTime:       &until,
+	})
+	if err != nil {
+		return nil, err
+	}
 	return &StampService{
 		employmentId: employmentId,
 		fromTime:     since,
 		toTime:       until,
 		stampRepo:    stampRepo,
-	}
+		stamps:       stamps,
+	}, nil
 }
 
-func (wr *StampService) GetWorkTime() (time.Duration, error) {
-	duration := time.Duration(0)
-
-	stamps, err := wr.stampRepo.List(&StampQuery{
-		EmploymentId: &wr.employmentId,
-		FromTime:     &wr.fromTime,
-		ToTime:       &wr.toTime,
-	})
-	if err != nil {
-		return 0, err
+func (ss *StampService) GetWorkTime(fromTime, toTime time.Time) (time.Duration, error) {
+	if fromTime.After(toTime) {
+		return 0, errInvalidTimeRange{}
 	}
+	duration := time.Duration(0)
+	stamps := ss.filterStamps(fromTime, toTime)
 
-	workStatusAtStart, err := GetWorkStatus(wr.fromTime, wr.employmentId, wr.stampRepo)
+	workStatusAtStart, err := GetWorkStatus(ss.fromTime, ss.employmentId, ss.stampRepo)
 	if err != nil {
 		return 0, err
 	}
 	startIdx := 0
 	if *workStatusAtStart == WorkStatusWorking {
 		startIdx = 1
-		duration += stamps[0].StampedAt.Sub(wr.fromTime)
+		duration += stamps[0].StampedAt.Sub(ss.fromTime)
 	}
 
 	for i := startIdx; i+1 < len(stamps); i += 2 {
 		duration += stamps[i+1].StampedAt.Sub(stamps[i].StampedAt)
 	}
 
-	workStatusAtEnd, err := GetWorkStatus(wr.toTime, wr.employmentId, wr.stampRepo)
+	workStatusAtEnd, err := GetWorkStatus(ss.toTime, ss.employmentId, ss.stampRepo)
 	if err != nil {
 		return 0, err
 	}
 	if *workStatusAtEnd == WorkStatusWorking {
-		duration += wr.toTime.Sub(stamps[len(stamps)-1].StampedAt)
+		duration += ss.toTime.Sub(stamps[len(stamps)-1].StampedAt)
 	}
 
 	return duration, nil
+}
+
+func (ss *StampService) filterStamps(fromTime, toTime time.Time) []*Stamp {
+	var stamps []*Stamp
+	for _, stamp := range ss.stamps {
+		if stamp.StampedAt.After(fromTime) && stamp.StampedAt.Before(toTime) {
+			stamps = append(stamps, stamp)
+		}
+	}
+	return stamps
+}
+
+type errInvalidTimeRange struct{}
+
+func (e errInvalidTimeRange) Error() string {
+	return "invalid time range"
 }
