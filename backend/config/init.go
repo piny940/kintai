9d42a7b1f3053bbe2e9766ceb58999e61d3d9a7a
piny940 <an.s.shoma@gmail.com>
2023-11-10 16:18:18 +0900

    不要なコードを削除

--- backend/config/init.go
+++ backend/config/init.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
@@ -11,8 +10,6 @@ import (
 var c *viper.Viper
 
 func Init(env string) {
-	fmt.Println(time.Location())
-
 	godotenv.Load(fmt.Sprintf(".env.%s", env))
 	c = viper.New()
 	c.SetConfigFile("yaml")
