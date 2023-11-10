e8578e22bad0a89388ee70d432d87722419c688c
mikan <83708535+piny940@users.noreply.github.com>
2023-11-10 15:00:05 +0900

    月間の勤務時間一覧を表示できるようにする

--- frontend/src/pages/companies/[company_id]/work_reports/[month].tsx
+++ frontend/src/pages/companies/[company_id]/work_reports/[month].tsx
@@ -4,10 +4,7 @@ import { useRouter } from 'next/router'
 
 const WorkReportShow = (): JSX.Element => {
   const router = useRouter()
-  console.log(router)
   const monthStr = router.query.month as string
-  console.log(monthStr)
-  console.log(Number(monthStr))
   const year = Number(monthStr) / 100
   const month = Number(monthStr) % 100
   return <MonthReport month={dayjs().year(year).month(month)} />
