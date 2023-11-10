03168815fb88005cd7022388e849600d4777b57d
mikan <83708535+piny940@users.noreply.github.com>
2023-11-10 15:27:37 +0900

    会社名を表示

--- frontend/src/containers/DateReport.tsx
+++ frontend/src/containers/DateReport.tsx
@@ -1,13 +1,25 @@
+import { useGetCompanyQuery } from '@/graphql/types'
+import { useCompanyId } from '@/hooks/calendar'
 import { Dayjs } from 'dayjs'
+import Error from 'next/error'
 
 export type DateReportProps = {
   date: Dayjs
 }
 
 const DateReport = ({ date }: DateReportProps): JSX.Element => {
+  const companyId = useCompanyId()
+  const { data: companyData, error } = useGetCompanyQuery({
+    variables: { id: companyId },
+  })
+
+  if (error) return <Error statusCode={404} />
+  if (!companyData?.company) return <>loading...</>
   return (
     <div className="">
-      <h1>勤務実績 - {date.format('YYYY年M月D日')}</h1>
+      <h1>
+        {date.format('YYYY年M月D日')} 勤務実績 - {companyData.company.name}
+      </h1>
       <table className="table">
         <thead>
           <tr>
