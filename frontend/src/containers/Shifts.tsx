f298ba0ead4766046bbcf5ecca62d606460e38f8
piny940 <an.s.shoma@gmail.com>
2023-11-18 22:55:21 +0900

    確定シフト閲覧ページを作成

---
+++ frontend/src/containers/Shifts.tsx
@@ -0,0 +1,7 @@
+import { memo } from 'react'
+
+const Shifts = (): JSX.Element => {
+  return <div>Shifts</div>
+}
+
+export default memo(Shifts)
---
+++ frontend/src/pages/companies/[company_id]/shifts/index.tsx
@@ -0,0 +1,7 @@
+import Shifts from '@/containers/Shifts'
+
+const ShiftsIndexPage = (): JSX.Element => {
+  return <Shifts />
+}
+
+export default ShiftsIndexPage
